67fd75dc1e4ce59ca057f2a9f484625d86aebd32
Gh0st184 <tu5s0qg>
2021-12-05 23:54:19 +0300

    lesson-4

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
@@ -20,9 +20,40 @@ def login(request):
     context = {
         'login_form': login_form
     }
-    return render(request, '', context)
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        register_form = ShopUserRegisterForm()
+    context = {
+        'register_form': register_form
+    }
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+       'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', context)
