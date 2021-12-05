58dc473d2858ca0617644fe632612e29ac69393f
Gh0st184 <tu5s0qg>
2021-12-06 01:30:54 +0300

    lesson-5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import json
 from mainapp.models import Product, ProductCategory
 
@@ -18,8 +18,28 @@ def contact(request):
 
 
 def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category_item = {
+                'name': 'все',
+                'pk': 0
+            }
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category__pk=pk)
+        context = {
+            'links_menu': ProductCategory.objects.all(),
+            'title': 'продукты',
+            'category_item': category_item,
+            'products': products_list
+        }
+        return render(request, "mainapp/products_list.html", context)
+
     context = {
         'links_menu': ProductCategory.objects.all(),
         'title': 'продукты'
     }
+
     return render(request, "mainapp/products.html", context)
