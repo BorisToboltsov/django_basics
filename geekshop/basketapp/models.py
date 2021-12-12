826ba1b21bb8f3853671e7247af6e411a1f0e2fa
Gh0st184 <tu5s0qg>
2021-12-12 21:31:48 +0300

    lesson-6

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -11,3 +11,17 @@ class Basket(models.Model):
 
     add_datetime = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def product_cost(self):
+        return self.quantity * self.product.price
+
+    @property
+    def total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    @property
+    def total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.product_cost, _items)))
